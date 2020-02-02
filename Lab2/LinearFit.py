7c4c681b4cd6eebdecb10dad43ac6ad0f60253b3
Andrew Lewis <andrewtaitelewis@gmail.com>
2020-02-02 17:27:18 -0500

    Added Juliann's calibration fit code, Turned two subplots into one plot

--- Lab2/LinearFit.py
+++ Lab2/LinearFit.py
@@ -43,10 +43,7 @@ def main():
 
 
 
-
-
     #Our Figure of A1
-    #Plot and residuals, therefore two subplots
     def figure1():
         '''Plots Figure 1 '''
         fig,(ax1,ax2) = plt.subplots(2,1)
@@ -75,7 +72,8 @@ def main():
 
         plt.savefig('A1')
         return
-
+        #Figure for A2
+    #Our Figure of A2
     def figure2():
         '''Plots Figure 2 '''
         fig,(ax1,ax2) = plt.subplots(2,1)
@@ -104,9 +102,68 @@ def main():
 
         plt.savefig('A2')
         return
+    #Linear fit and residuals
+    def linearCalibration():
+        #Data manipulation
+
+        bits= np.array([0,5,10,20,30,40,50,65,75,90,100,115,130,150,175,200,215,230,245,255])
+        volts = np.array([0.004,0.102,0.200,0.397,0.594,0.791,0.988,1.283,1.480,1.775,1.972,2.267,2.562,2.955,3.45,3.94,4.23,4.53,4.82,5.02])
+        fit = np.polyfit(bits,volts,1)
+
+        voltserr = [0.001]*20
+        def residualNumbers(x,y):
+            residualValues = x-y
+            return residualValues
+
+        def slope(x,y):
+            a = (y[-1] - y[0])/(x[-1] - x[0])
+            return a
+        def func(x,a,b):
+            return a*x + b
+        y = func(bits,slope(bits,volts),0.004)
+
+        popt, pcov = curve_fit(func, bits, volts)
+
+        #Now for error bars
+        error = np.array([0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.01,0.01,0.01,0.01,0.01,0.01,])
+        #Now all the plotting of our figures
+
+        fig,(ax1,ax2) = plt.subplots(2,1)
+        #First subplot
+        ax1.plot(bits, func(bits,*popt), 'r-',label ='Fitted Number Voltage Relationship')
+        ax1.plot(bits,volts,'o',label = 'Data Points from Multimeter')
+        ax1.errorbar(bits,volts,yerr = error,fmt = '.',label = 'Error Bars')
+        ax1.set_title('Voltage Output for 8-bit Values', fontsize = 14)
+        ax1.set_xlabel('8-Bit values')
+        ax1.set_ylabel('Voltage [V]' )
+        ax1.legend()
+        ax1.set_facecolor((.97,.97,.97))
+        perr = np.sqrt(np.diag(pcov))
+
+        #Second subplot residuals
+        ax2.plot(bits,residualNumbers(volts,func(bits, *popt)), 'r.', label = "Voltage Output Residuals")
+        ax2.axhline(0,color='k')
+        ax2.set_title('Voltage Residuals',fontsize = 14)
+        ax2.axis(([-5,257,-0.2,0.2]))
+        ax2.set_xlabel('8 Bit Values')
+        ax2.set_ylabel('Residuals')
+        ax2.legend()
+        ax2.set_facecolor((.97,.97,.97))
+
+        #Figure
+        fig.align_ylabels()
+        fig.tight_layout()
+        fig.savefig('LinearFit')
+
+
+
+
+
+
     #Ploting our figures
     figure1()
     figure2()
+    linearCalibration()
     #Our figure of A2
     '''
     plt.figure()
