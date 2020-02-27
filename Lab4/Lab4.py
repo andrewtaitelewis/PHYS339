5992c1c1144542cbd02183e0804666c6498f3df5
Andrew Lewis <andrewtaitelewis@gmail.com>
2020-02-27 16:05:57 -0500

    Attempted to use the partial fitting, however scipy was a better use

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,20 +1,39 @@
 #Importing our modules
 import numpy as np 
 import matplotlib.pyplot as plt 
+from scipy import optimize as opt
+
 
 #Importing that CSV 
 Lab4steps = np.loadtxt(r"C:\Users\Andrew\Desktop\Classes\PHYS 339\Lab4\Lab4steps.csv",delimiter = ',')
 Lab4Vals = np.loadtxt(r"C:\Users\Andrew\Desktop\Classes\PHYS 339\Lab4\Lab4Vals.csv", delimiter = ',')
 
-
 #Our Functions 
-def malusLawI(I0,theta):
+def malusLawI(theta,V0,phi):
 
-    return I0*(np.cos(theta))**2
+    theta = theta*0.0174533
+    phi = phi*0.0174533
+    return V0*(np.cos(theta+phi))**2
 
 
-def malusFitter():
-    '''Okay mhmmm this is gonna be a doozie '''
+def malusFitter(voltageData,variance,initialPhiGuess,iterationNum):
+    '''Fits Malus' law to a set of data 
+    Params:
+    -------
+    voltageData: our raw voltage data
+    variance: the variance on a given data point
+    initialPhiGuess: our original guess for phi
+    iterationNum: the number of iterations we we will be undergoing to sole
+
+    Returns:
+    --------
+    phi: a fitted value for phi
+    V0 : a fitted value for V0
+    '''
+
+
+
+    
 
     #More functions to fit, from appendix B
 
@@ -76,7 +95,7 @@ def malusFitter():
             #Updating steps
             steps += 1
 
-        return (12*V0**2)*sum1 -(16*V0**2)*sum2 - 4*V0*sum3 + (8*V0**2)*sum4
+        return (12*V0**2)*sum1 -(16*V0**2)*sum2 - 4*V0*sum3 + (8*V0)*sum4
 
 
 
@@ -102,20 +121,20 @@ def malusFitter():
 
         #The reason for the multiplication by 0.0174533 is because np.cos uses rads
         for i,j in zip(voltageData,variance):
-            numerator += (i* np.cos(steps*0.0174533+phi*0.0174533)**2)/j
-            denominator += (np.cos(i*0.0174533+phi*0.0174533)**4)/j
+            numerator += (i*(np.cos(steps*0.0174533+phi*0.0174533))**2)/j
+            denominator += (np.cos(i*0.0174533+phi*0.0174533))**4/j
             steps += 1
 
         return numerator/denominator
 
-    def newPhi(oldPhi,voltageData,V0,phi,varaince):
+    def newPhi(oldPhi,voltageData,V0,varaince):
         '''Returns an estimation of phi based on the old value of phi 
         Params:
         -------
         oldPhi: the previous guess for phi
         voltageData: the voltage data we are fitting to
         V0: guess for V0
-        phi: the initial angle difference between our polarisers
+        
         variance: the variance in our data, think sigma**2
 
         Returns:
@@ -123,25 +142,79 @@ def malusFitter():
         A new value of phi
          '''
 
-        returnedValue = oldPhi - X2P(voltageData,V0,phi,varaince)/X2P2(voltageData,V0,phi,varaince)
+        returnedValue = oldPhi - X2P(voltageData,V0,oldPhi,varaince)/X2P2(voltageData,V0,oldPhi,varaince)
         return returnedValue
-    return
+
+
+    #------------------------------------------------
+    returnedPhis = []
+    returnedV0s = []
+    #initalGuess for V0
+    phi = initialPhiGuess
+    V0Val = V0(voltageData,phi,variance) 
+
+   
+    #Now move to finding the values
+    for i in range(iterationNum):
+        returnedV0s.append(V0Val)
+        returnedPhis.append(phi)
+        newphi = newPhi(phi,voltageData,V0Val,variance)
+        phi = newphi
+        V0Val = V0(voltageData,phi,variance)
+
+        if(i%100 == 0):
+            print(i)
+            print(V0Val)
+
+
+    
+
+
+    return returnedPhis,returnedV0s
 
 #----------------------------------
 
 #Section 2.1 - Malus' Law
 
-#X and Y axis
-xAxis = Lab4steps
-yAxis = Lab4Vals
+#Importing more CSV
+data21 = np.loadtxt(r"C:\Users\Andrew\Desktop\Classes\PHYS 339\Lab4\AndysVoltageVals.csv", delimiter = ',')
 
-#Normalize the yAxis
-yNormalized = yAxis/(max(yAxis))
+#Finding the maximum value
+maxVal = 0
+for i in data21:
+    
+    for j in i:
+        if j > maxVal:
+            maxVal = j
+        
+#Normalizing the data
+for i in range(len(data21)):
 
-#Our Malus's Law     1 Step = 1 Degree
+    for j in range(len(data21[i])):
+        data21[i][j] = data21[i][j]/maxVal
+ 
+#Getting the x range of the device
+xs = np.linspace(0,len(data21[0]) -1, len(data21[0]))
 
-#Plot the figure
-plt.plot(xAxis,yNormalized)
+#get the means and std of the data
+means = []
+std = []
+for i in data21.T :
+    means.append(np.mean(i))
+    std.append(np.std(i))
 
+
+values,cov  = opt.curve_fit(malusLawI,xs,means)
+vnaught = values[0]
+phi = values[1]
+
+
+
+plt.plot(xs,malusLawI(xs,vnaught,phi))
+
+plt.errorbar(xs,means,yerr = std, fmt =  '.')
 plt.show()
+
+
+#We will now try with scipy.optimize
 
